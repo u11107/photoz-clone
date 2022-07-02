f01fff116f4cd990baa64aad4f094ffb1b497483
Rinat <eenot84@yandex.ru>
2022-07-02 15:46:01 +0300

    Start

---
+++ src/main/java/com/home/rinat/photozclone/PhotozCloneApplication.java
@@ -0,0 +1,13 @@
+package com.home.rinat.photozclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotozCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/home/rinat/photozclone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.rinat.photozclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
